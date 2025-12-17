8057ea70f7d49912417e16987195519fa6ed4fd4
Giuseppe <pistoia702@gmail.com>
2025-12-17 11:35:51 +0100

    fix: Rimuovo import useRouter non utilizzato

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
 export default function PaymentSuccessPage() {
